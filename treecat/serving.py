Fix a scaling bug in server.logprob; add a failing test

treecat/serving.py
@@ -27,7 +27,8 @@ def make_posterior(prior, suffstats):
       A numpy array of normalized probabilities.
     '''
     probs = prior + suffstats.astype(np.float32)
-    totals = probs.sum(axis=0).reshape((1, ) + probs.shape[1:])
+    axes = tuple(range(1, len(probs.shape)))
+    totals = probs.sum(axis=axes, keepdims=True)
     assert (totals > 0).all()
     probs /= totals
     return probs
@@ -78,9 +79,10 @@ def build_graph(tree, suffstats, config, num_rows):
     assert suffstats['vert_ss'].shape == (V, M)
     assert suffstats['edge_ss'].shape == (E, M, M)
     assert suffstats['feat_ss'].shape == (V, C, M)
-    feat_probs = tf.Variable(make_posterior(feat_prior, suffstats['feat_ss']))
     vert_probs = tf.Variable(make_posterior(vert_prior, suffstats['vert_ss']))
     edge_probs = tf.Variable(make_posterior(edge_prior, suffstats['edge_ss']))
+    feat_probs = tf.Variable(
+        make_posterior(feat_prior, suffstats['feat_ss']) * C)
 
     COUNTERS.footprint_serving_feat_probs = sizeof(feat_probs)
     COUNTERS.footprint_serving_vert_probs = sizeof(vert_probs)
@@ -262,5 +264,5 @@ class TreeCatServer(object):
         assert mask.shape[0] == self._tree.num_vertices
         session = self._get_session(num_rows)
         result = session.run('logprob:0', {'data:0': data, 'mask:0': mask})
-        assert result.shape == [num_rows]
+        assert result.shape == (num_rows, )
         return result

treecat/serving_test.py
@@ -38,7 +38,6 @@ def test_server_sample(model):
         assert np.allclose(samples[:, mask], TINY_DATA[:, mask])
 
 
-@pytest.mark.xfail
 def test_server_logprob(model):
     N, V = TINY_DATA.shape
     server = TreeCatServer(model['tree'], model['suffstats'], TINY_CONFIG)
@@ -51,3 +50,16 @@ def test_server_logprob(model):
         assert logprob.shape == (N, )
         assert np.isfinite(logprob).all()
         assert (logprob < 0.0).all()  # Assuming features are discrete.
+
+
+@pytest.mark.xfail
+def test_server_logprob_total(model):
+    N, V = TINY_DATA.shape
+    C = TINY_CONFIG['num_categories']
+    server = TreeCatServer(model['tree'], model['suffstats'], TINY_CONFIG)
+    factors = [range(C)] * V
+    data = np.array(list(itertools.product(*factors)), dtype=np.int32)
+    mask = np.array([True] * V, dtype=np.bool_)
+    logprob = server.logprob(data, mask)
+    total = np.exp(np.logaddexp.reduce(logprob))
+    assert abs(total - 1.0) < 1e-6, total