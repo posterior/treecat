Fix normalization bug in np_engine server

treecat/np_engine.py
@@ -242,18 +242,20 @@ class NumpyServer(ServerBase):
     def _propagate(self, data, mask, task):
         N = data.shape[0]
         V, E, M, C = self._VEMC
+        factor_observed = self._factors['observed']
         factor_observed_latent = self._factors['observed_latent']
         factor_latent = self._factors['latent']
         factor_latent_latent = self._factors['latent_latent']
 
         messages = np.zeros([V, N, M], dtype=np.float32)
         messages[...] = factor_latent[:, np.newaxis, :]
-        messages_scale = np.zeros([V, N], dtype=np.float32)
+        logprob = np.zeros([N], dtype=np.float32)
         for v, parent, children in reversed(self._schedule):
             message = messages[v, :, :]
             # Propagate upward from observed to latent.
             if mask[v]:
                 message *= factor_observed_latent[v, data[:, v], :]
+                logprob += np.log(factor_observed[v, data[:, v]])
             # Propagate latent state inward from children to v.
             for child in children:
                 e = self._tree.find_tree_edge(child, v)
@@ -262,15 +264,15 @@ class NumpyServer(ServerBase):
                 else:
                     trans = factor_latent_latent[e, :, :].T
                 message *= np.dot(messages[child, :, :], trans)
-            messages_scale[v, :] = message.max(axis=1)
-            message /= messages_scale[v, :, np.newaxis]
+            message_scale = message.max(axis=1)
+            message /= message_scale[:, np.newaxis]
+            logprob += np.log(message_scale)
 
         if task == 'logprob':
             # Aggregate the total logprob.
             root, parent, children = self._schedule[0]
             assert parent is None
-            logprob = np.log(messages[root, :, :].sum(axis=1))
-            logprob += np.log(messages_scale).sum(axis=0)
+            logprob += np.log(messages[root, :, :].sum(axis=1))
             return logprob
 
         elif task == 'sample':

treecat/serving_test.py
@@ -105,7 +105,7 @@ def test_server_logprob_shape(engine, model):
 
 
 @pytest.mark.parametrize('engine', [
-    pytest.mark.xfail('numpy'),
+    'numpy',
     pytest.mark.xfail('tensorflow'),
 ])
 def test_server_logprob_negative(engine, model):
@@ -119,14 +119,15 @@ def test_server_logprob_negative(engine, model):
     for mask in itertools.product(*factors):
         mask = np.array(mask, dtype=np.bool_)
         logprob = server.logprob(TINY_DATA, mask)
-        assert (logprob < 0.0).all()  # Assuming features are discrete.
+        abstol = 1e-5
+        assert (logprob <= abstol).all()  # Assuming features are discrete.
 
 
 @pytest.mark.parametrize('engine', [
-    pytest.mark.xfail('numpy'),
+    'numpy',
     pytest.mark.xfail('tensorflow'),
 ])
-def test_server_logprob_is_normalized(engine, model):
+def test_server_logprob_normalized(engine, model):
     config = TINY_CONFIG.copy()
     config['engine'] = engine
     server = serve_model(model['tree'], model['suffstats'], config)
@@ -140,6 +141,7 @@ def test_server_logprob_is_normalized(engine, model):
     logprob = server.logprob(data, mask)
     logtotal = np.logaddexp.reduce(logprob)
     assert abs(logtotal) < 1e-6, logtotal
+    assert logtotal == pytest.approx(0.0, abs=1e-5)
 
 
 @pytest.mark.parametrize('engine', [
@@ -152,7 +154,7 @@ def test_server_gof(engine, model):
     server = serve_model(model['tree'], model['suffstats'], config)
 
     # Generate samples.
-    N = 10000  # Number of samples.
+    N = 20000  # Number of samples.
     C = config['num_categories']
     V = TINY_DATA.shape[1]
     empty_data = np.zeros([N, V], dtype=np.int32)
@@ -174,4 +176,5 @@ def test_server_gof(engine, model):
     # Check accuracy using Pearson's chi-squared test.
     counts = np.array([counts[key] for key in keys])
     probs = np.array([probs[key] for key in keys])
+    probs /= probs.sum()  # Test normalization elsewhere.
     assert 1e-2 < multinomial_goodness_of_fit(probs, counts, total_count=N)