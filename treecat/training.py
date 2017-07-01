Fix bug in logprob calculation; more tests pass

treecat/training.py
@@ -34,20 +34,19 @@ def count_pairs(assignments, v1, v2, M):
     return np.bincount(pairs, minlength=M * M).reshape((M, M))
 
 
-def logprob_dm(counts, prior, axis=None):
-    """Non-normalized log probability of a Dirichlet-Multinomial distribution.
+def logprob_dc(counts, prior, axis=None):
+    """Non-normalized log probability of a Dirichlet-Categorical distribution.
 
     See https://en.wikipedia.org/wiki/Dirichlet-multinomial_distribution
     """
-    return (gammaln(counts + prior).sum(axis) -
-            gammaln(counts + 1.0).sum(axis))
+    return gammaln(counts + prior).sum(axis)
 
 
 def sample_from_probs(probs):
     # Note: np.random.multinomial is faster than np.random.choice,
     # but np.random.multinomial is pickier about non-normalized probs.
     try:
-        # This is equivalent to: np.random.choice(M, p=probs)
+        # This is equivalent to: np.random.choice(len(probs), p=probs)
         return np.random.multinomial(1, probs).argmax()
     except ValueError:
         COUNTERS.np_random_multinomial_value_error += 1
@@ -141,8 +140,8 @@ class TreeCatTrainer(object):
                 trans = edge_probs[e, :, :]
                 if v > child:
                     trans = trans.T
-                message *= np.dot(
-                    trans, messages[child, :] / vert_probs[child, :])
+                message *= np.dot(trans,
+                                  messages[child, :] / vert_probs[child, :])
                 message /= vert_probs[v, :]
                 message /= message.sum()  # For numerical stability only.
 
@@ -174,12 +173,12 @@ class TreeCatTrainer(object):
                     len(self._assigned_rows))
         V, E, K, M = self._VEKM
         assignments = self.assignments[sorted(self._assigned_rows), :]
-        vertex_logits = logprob_dm(self._vert_ss, self._vert_prior, axis=1)
+        vertex_logits = logprob_dc(self._vert_ss, self._vert_prior, axis=1)
         edge_logits = np.zeros([K], np.float32)
         for k, v1, v2 in self.tree.tree_grid.T:
             counts = count_pairs(assignments, v1, v2, M)
             # This is the most expensive part of tree sampling:
-            edge_logits[k] = (logprob_dm(counts, self._edge_prior) -
+            edge_logits[k] = (logprob_dc(counts, self._edge_prior) -
                               vertex_logits[v1] - vertex_logits[v2])
 
         # Sample the tree.
@@ -200,14 +199,16 @@ class TreeCatTrainer(object):
         This is mainly useful for testing goodness of fit of the category
         kernel.
         """
+        assert len(self._assigned_rows) == self.assignments.shape[0]
         V, E, K, M = self._VEKM
-        vertex_logits = logprob_dm(self._vert_ss, self._vert_prior, axis=1)
+        vertex_logits = logprob_dc(self._vert_ss, self._vert_prior, axis=1)
         logprob = vertex_logits.sum()
         for e, v1, v2 in self.tree.tree_grid.T:
-            logprob += (logprob_dm(self._edge_ss[e, :, :], self._edge_prior) -
+            logprob += (logprob_dc(self._edge_ss[e, :, :], self._edge_prior) -
                         vertex_logits[v1] - vertex_logits[v2])
         for v in range(V):
-            logprob += logprob_dm(self._feat_ss[v], self._feat_prior)
+            logprob += logprob_dc(self._feat_ss[v], self._feat_prior)
+            logprob -= logprob_dc(self._feat_ss[v].sum(0), self._vert_prior)
         return logprob
 
     def finish(self):

treecat/training_test.py
@@ -12,7 +12,23 @@ from treecat.structure import TreeStructure
 from treecat.testutil import TINY_CONFIG
 from treecat.training import TreeCatTrainer
 from treecat.training import get_annealing_schedule
+from treecat.training import sample_from_probs
 from treecat.training import train_model
+
+
+@pytest.mark.parametrize('size', range(1, 10))
+def test_sample_from_probs(size):
+    np.random.seed(size)
+    probs = np.exp(2 * np.random.random(size)).astype(np.float32)
+    probs /= probs.sum()
+    counts = np.zeros(size, dtype=np.int32)
+    num_samples = 2000 * size
+    for _ in range(num_samples):
+        counts[sample_from_probs(probs)] += 1
+    print(counts)
+    print(probs * num_samples)
+    gof = multinomial_goodness_of_fit(probs, counts, num_samples, plot=True)
+    assert 1e-2 < gof
 
 
 def test_get_annealing_schedule():
@@ -110,14 +126,15 @@ def hash_assignments(assignments):
     (1, 2, 2, 3),
     (1, 3, 2, 2),
     (1, 4, 2, 2),
-    (2, 1, 2, 2),  # Flaky.
+    pytest.mark.xfail((2, 1, 1, 2)),
+    (2, 1, 2, 2),
     pytest.mark.xfail((2, 1, 2, 3)),
     pytest.mark.xfail((2, 2, 1, 2)),
-    pytest.mark.xfail((2, 2, 2, 2)),
-    pytest.mark.xfail((2, 3, 2, 2)),
+    (2, 2, 2, 2),
+    (2, 3, 2, 2),
     pytest.mark.xfail((3, 1, 1, 2)),
     (3, 1, 2, 2),
-    (4, 1, 1, 2),
+    pytest.mark.xfail((4, 1, 1, 2)),
 ])
 def test_assignment_sampler_gof(N, V, C, M):
     config = DEFAULT_CONFIG.copy()
@@ -148,15 +165,15 @@ def test_assignment_sampler_gof(N, V, C, M):
         else:
             counts[key] = 1
             logprobs[key] = trainer.logprob()
-    print('logprobs:')
-    for key, val in sorted(logprobs.items()):
-        print('{:0.3g}\t{}'.format(val, key))
     assert len(counts) == M**(N * V)
 
     # Check accuracy using Pearson's chi-squared test.
-    keys = counts.keys()
+    keys = sorted(counts.keys())
     counts = np.array([counts[k] for k in keys], dtype=np.int32)
     probs = np.exp(np.array([logprobs[k] for k in keys]))
     probs /= probs.sum()
+    print('Actual\tExpected\tAssignment')
+    for count, prob, key in zip(counts, probs, keys):
+        print('{:}\t{:0.1f}\t{}'.format(count, prob * num_samples, key))
     gof = multinomial_goodness_of_fit(probs, counts, num_samples, plot=True)
     assert 1e-2 < gof