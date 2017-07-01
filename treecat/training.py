Fix bug in Trainer.logprob(), all gof tests pass

treecat/training.py
@@ -34,12 +34,12 @@ def count_pairs(assignments, v1, v2, M):
     return np.bincount(pairs, minlength=M * M).reshape((M, M))
 
 
-def logprob_dc(counts, prior, axis=None):
+def logprob_dc(counts_plus_prior, axis=None):
     """Non-normalized log probability of a Dirichlet-Categorical distribution.
 
     See https://en.wikipedia.org/wiki/Dirichlet-multinomial_distribution
     """
-    return gammaln(counts + prior).sum(axis)
+    return gammaln(counts_plus_prior).sum(axis)
 
 
 def sample_from_probs(probs):
@@ -173,12 +173,12 @@ class TreeCatTrainer(object):
                     len(self._assigned_rows))
         V, E, K, M = self._VEKM
         assignments = self.assignments[sorted(self._assigned_rows), :]
-        vertex_logits = logprob_dc(self._vert_ss, self._vert_prior, axis=1)
+        vertex_logits = logprob_dc(self._vert_ss + self._vert_prior, axis=1)
         edge_logits = np.zeros([K], np.float32)
         for k, v1, v2 in self.tree.tree_grid.T:
             counts = count_pairs(assignments, v1, v2, M)
             # This is the most expensive part of tree sampling:
-            edge_logits[k] = (logprob_dc(counts, self._edge_prior) -
+            edge_logits[k] = (logprob_dc(counts + self._edge_prior) -
                               vertex_logits[v1] - vertex_logits[v2])
 
         # Sample the tree.
@@ -201,14 +201,14 @@ class TreeCatTrainer(object):
         """
         assert len(self._assigned_rows) == self.assignments.shape[0]
         V, E, K, M = self._VEKM
-        vertex_logits = logprob_dc(self._vert_ss, self._vert_prior, axis=1)
+        vertex_logits = logprob_dc(self._vert_ss + self._vert_prior, axis=1)
         logprob = vertex_logits.sum()
         for e, v1, v2 in self.tree.tree_grid.T:
-            logprob += (logprob_dc(self._edge_ss[e, :, :], self._edge_prior) -
+            logprob += (logprob_dc(self._edge_ss[e, :, :] + self._edge_prior) -
                         vertex_logits[v1] - vertex_logits[v2])
         for v in range(V):
-            logprob += logprob_dc(self._feat_ss[v], self._feat_prior)
-            logprob -= logprob_dc(self._feat_ss[v].sum(0), self._vert_prior)
+            feat_probs = self._feat_ss[v] + self._feat_prior
+            logprob += logprob_dc(feat_probs) - logprob_dc(feat_probs.sum(0))
         return logprob
 
     def finish(self):