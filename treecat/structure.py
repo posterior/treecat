f45d7f9570a26857531acc9932fd9bacb2a9fb7d
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-07-02 15:09:46 -0700

    Avoid np.binom precision errors

--- treecat/structure.py
+++ treecat/structure.py
@@ -325,7 +325,7 @@ def sample_tree(grid, edge_logits, edges, steps=1):
             np.exp(valid_probs, out=valid_probs)
             total_prob = valid_probs.sum()
             if total_prob > 0:
-                valid_probs /= total_prob
+                valid_probs *= 0.9999995 / total_prob  # Avoid np.binom errors.
                 k2 = valid_edges[sample_from_probs(valid_probs)]
             else:
                 k2 = k1
