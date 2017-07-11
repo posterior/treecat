9035ce086ca6a0566bb6b6eadf41d33f5013b611
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-07-10 19:49:40 -0700

    Drop obsolete functions tune_epochs(), tune_clusters()

--- treecat/validate.py
+++ treecat/validate.py
@@ -21,6 +21,24 @@ Stats = namedtuple('Stats', ['logprob', 'l1_loss'])
 
 
 def split_data(ragged_index, data, num_parts):
+    """Split a dataset for unsupervised crossvalidation.
+
+    This splits a dataset into num_parts disjoint parts by randomly holding out
+    cells. Note that whereas supervised crossvalidation typically holds out
+    entire rows, our unsupervised crossvalidation is intended to evaluate a
+    model of the full joint distribution.
+
+    Args:
+        ragged_index: A [V+1]-shaped numpy array of indices into the ragged
+            data array, where V is the number of features.
+        data: A [N, R]-shaped ragged nummpy array of multinomial count data,
+            where N is the number of rows, and R is ragged_index[-1].
+        num_parts: An integer, the number of folds in n-fold crossvalidation.
+
+    Returns:
+        A num_parts-long list of arrays shaped like data, each with some of its
+        cells zeroed out.
+    """
     N = data.shape[0]
     V = ragged_index.shape[0] - 1
     holdouts = np.random.randint(num_parts, size=(N, V))
@@ -70,42 +88,6 @@ def plan_crossvalidation(key, ragged_index, data, config):
         sub_config['seed'] += sub_seed
         tasks.append((key, ragged_index, counts, data, part, sub_config))
     return tasks
-
-
-@parsable
-def tune_epochs(dataset_path, result_path, *epochs, **options):
-    """Tune learning_epochs by crossvalidating posterior predictive."""
-    keys = list(map(int, epochs))
-    tasks = []
-    dataset = pickle_load(dataset_path)
-    ragged_index = dataset['schema']['ragged_index']
-    data = dataset['data']
-    for key in keys:
-        config = make_config(learning_epochs=key, **options)
-        tasks += plan_crossvalidation(key, ragged_index, data, config)
-    print('tuning via {} tasks'.format(len(tasks)))
-    result = parallel_map(_crossvalidate, tasks)
-    for line in sorted(result):
-        print(line)
-    pickle_dump(result, result_path)
-
-
-@parsable
-def tune_clusters(dataset_path, result_path, *clusters, **options):
-    """Tune model_num_clusters by crossvalidating posterior predictive."""
-    keys = list(map(int, clusters))
-    tasks = []
-    dataset = pickle_load(dataset_path)
-    ragged_index = dataset['schema']['ragged_index']
-    data = dataset['data']
-    for key in keys:
-        config = make_config(model_num_clusters=key, **options)
-        tasks += plan_crossvalidation(key, ragged_index, data, config)
-    print('tuning via {} tasks'.format(len(tasks)))
-    result = parallel_map(_crossvalidate, tasks)
-    for line in sorted(result):
-        print(line)
-    pickle_dump(result, result_path)
 
 
 @parsable
--- treecat/validate_test.py
+++ treecat/validate_test.py
@@ -7,43 +7,9 @@ import os
 from treecat.generate import generate_dataset_file
 from treecat.testutil import TESTDATA
 from treecat.testutil import tempdir
-from treecat.validate import tune_clusters
 from treecat.validate import tune_csv
-from treecat.validate import tune_epochs
 
 PARAM_CSV = os.path.join(TESTDATA, 'tuning.csv')
-
-
-def test_tune_epochs():
-    dataset_path = generate_dataset_file(5, 7)
-    with tempdir() as dirname:
-        result_path = os.path.join(dirname, 'tune_epochs.pkz')
-        tune_epochs(
-            dataset_path,
-            result_path,
-            2,
-            3,
-            4,
-            5,
-            6,
-            model_ensemble_size=3,
-            model_num_clusters=5)
-
-
-def test_tune_clusters():
-    dataset_path = generate_dataset_file(5, 7)
-    with tempdir() as dirname:
-        result_path = os.path.join(dirname, 'tune_clusters.pkz')
-        tune_clusters(
-            dataset_path,
-            result_path,
-            2,
-            3,
-            4,
-            5,
-            6,
-            model_ensemble_size=3,
-            learning_epochs=5)
 
 
 def test_tune_csv():
