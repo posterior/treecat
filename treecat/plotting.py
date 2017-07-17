ed5542c9b3ad2b045b2389b4b4aa673c4b544c0b
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-07-16 20:44:42 -0700

    Move order_vertices to structure module

--- treecat/plotting.py
+++ treecat/plotting.py
@@ -5,9 +5,7 @@ from __future__ import print_function
 import numpy as np
 import scipy.linalg
 
-from treecat.structure import find_center_of_tree
-from treecat.structure import make_tree
-from treecat.structure import print_tree
+from treecat.structure import order_vertices
 
 
 def layout_tree(correlation):
@@ -58,66 +56,6 @@ def nx_plot_tree(server, node_size=200, **options):
     nx.draw(H, with_labels=True, node_size=node_size, **options)
 
 
-def order_features_v1(server):
-    """Sort features using print_tree."""
-    edges = server.estimate_tree
-    names = server.feature_names
-    order = np.empty(len(names), np.int32)
-    for v, name in enumerate(print_tree(edges, names).split()):
-        order[names.index(name)] = v
-    return order
-
-
-def order_features_v2(server):
-    """Sort features using greedy bin packing."""
-    edges = server.estimate_tree
-    grid = make_tree(edges)
-    root = find_center_of_tree(grid)
-
-    E = len(edges)
-    V = E + 1
-    neighbors = [set() for _ in range(V)]
-    for v1, v2 in edges:
-        neighbors[v1].add(v2)
-        neighbors[v2].add(v1)
-    orders = [None for v in range(V)]
-    seen = [False] * V
-    stack = [root]
-    seen[root] = True
-    while stack:
-        v = stack[-1]
-        done = True
-        for v2 in neighbors[v]:
-            if not seen[v2]:
-                stack.append(v2)
-                seen[v2] = True
-                done = False
-                break
-        if not done:
-            continue
-        lhs = []
-        rhs = []
-        parts = [(v2, orders[v2]) for v2 in neighbors[v]
-                 if orders[v2] is not None]
-        parts.sort(key=lambda x: len(x[1]))
-        for v2, part in parts:
-            if len(lhs) < len(rhs):
-                if part.index(v2) < len(part) / 2:
-                    part.reverse()
-                lhs += part
-            else:
-                if part.index(v2) > len(part) / 2:
-                    part.reverse()
-                rhs += part
-        orders[v] = lhs + [v] + rhs
-        stack.pop()
-
-    result = [None] * V
-    for v1, v2 in enumerate(orders[root]):
-        result[v2] = v1
-    return result
-
-
 def plot_circular(server, color='#4488aa'):
     """Plot a tree stucture with features arranged around a circle.
 
@@ -133,15 +71,15 @@ def plot_circular(server, color='#4488aa'):
     from matplotlib.patches import PathPatch
 
     # Extract ordered parameters to draw.
-    order = order_features_v2(server)
+    edges = server.estimate_tree
+    order = order_vertices(edges)
     feature_names = np.array(server.feature_names)[order]
     feature_density = server.feature_density()[order]
     observed_perplexity = server.observed_perplexity()[order]
     latent_perplexity = server.latent_perplexity()[order]
     # latent_correlation = server.latent_correlation()[order, :][:, order]
     edges = np.array(
-        [[order[v1], order[v2]] for v1, v2 in server.estimate_tree],
-        dtype=np.int32)
+        [[order[v1], order[v2]] for v1, v2 in edges], dtype=np.int32)
     alphas = 0.25 + 0.75 * feature_density
 
     V = len(feature_names)
--- treecat/structure.py
+++ treecat/structure.py
@@ -470,3 +470,61 @@ def print_tree(edges, feature_names, root=None):
             lines.append((len(stack), name))
     lines.reverse()
     return '\n'.join(['{}{}'.format('  ' * i, n) for i, n in lines])
+
+
+def order_vertices(edges):
+    """Sort vertices using greedy bin packing.
+
+    This is mainly useful for plotting and printing.
+
+    Args:
+      edges: A list of (vertex, vertex) pairs.
+
+    Returns:
+      A permutation of vertices for which edges are short.
+    """
+    grid = make_tree(edges)
+    root = find_center_of_tree(grid)
+
+    E = len(edges)
+    V = E + 1
+    neighbors = [set() for _ in range(V)]
+    for v1, v2 in edges:
+        neighbors[v1].add(v2)
+        neighbors[v2].add(v1)
+    orders = [None for v in range(V)]
+    seen = [False] * V
+    stack = [root]
+    seen[root] = True
+    while stack:
+        v = stack[-1]
+        done = True
+        for v2 in neighbors[v]:
+            if not seen[v2]:
+                stack.append(v2)
+                seen[v2] = True
+                done = False
+                break
+        if not done:
+            continue
+        lhs = []
+        rhs = []
+        parts = [(v2, orders[v2]) for v2 in neighbors[v]
+                 if orders[v2] is not None]
+        parts.sort(key=lambda x: len(x[1]))
+        for v2, part in parts:
+            if len(lhs) < len(rhs):
+                if part.index(v2) < len(part) / 2:
+                    part.reverse()
+                lhs = part + lhs
+            else:
+                if part.index(v2) > len(part) / 2:
+                    part.reverse()
+                rhs = rhs + part
+        orders[v] = lhs + [v] + rhs
+        stack.pop()
+
+    result = [None] * V
+    for v1, v2 in enumerate(orders[root]):
+        result[v2] = v1
+    return result
