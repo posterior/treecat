87c5dca7ba26e15ae6752613a2cc1e62b5264886
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-09-04 10:19:03 -0700

    Move config serialization helpers to config module

--- treecat/config.py
+++ treecat/config.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+# Changes to keys invalidates configs serialized by serialize_config.
 DEFAULT_CONFIG = {
     'seed': 0,
     'model_num_clusters': 32,
@@ -21,4 +22,22 @@ def make_config(**options):
             raise ValueError('Unknown option: {}. Try one of:\n  {}'.format(
                 key, '\n  '.join(sorted(config.keys()))))
         config[key] = int(value)
+    return config
+
+
+def serialize_config(config):
+    """Serialize a config dict to a short string for use in filenames."""
+    keys = sorted(config.keys())
+    assert keys == sorted(make_config().keys())
+    return '-'.join(str(int(config[key])) for key in keys)
+
+
+def deserialize_config(config_str):
+    """Deserialize a config dict form a short string."""
+    config = make_config()
+    keys = sorted(config.keys())
+    values = config_str.split('-')
+    assert len(keys) == len(values)
+    for key, value_str in zip(keys, values):
+        config[key] = int(value_str)
     return config
